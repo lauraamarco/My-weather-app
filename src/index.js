e8a2c97f1e3851b6a22acd66afc81fd72d403a8d
lauraamarco <lauraamarco22@gmail.com>
2024-06-19 22:13:36 +0200

    Setup apiKey and apiURL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "db2eaa70afcd7t05847a43o4d14ba820";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiURL);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
